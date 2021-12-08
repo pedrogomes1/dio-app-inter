4067574f5f9d06ae6fbd874a951e1ee458592488
Pedro <pedro-gomes17@hotmail.com>
2021-12-08 00:19:30 -0300

    chore: using globalErrors in index main file

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,12 +1,14 @@
 import express from 'express';
+
 import routes from './routes';
+import { globalErrors } from './middlewares/globalErrors';
 
 const app = express();
 const PORT = 3333;
 
 app.use(routes);
 
-app.get('/', (req, res) => res.send('Express + TypeScript Server'));
+app.use(globalErrors);
 
 app.listen(PORT, () => {
   console.log(`⚡️[server]: Server is running at http://localhost:${PORT}`);
