f5bfb07c0812851c56f065ee71a4e5337176128f
Pedro <pedro-gomes17@hotmail.com>
2021-12-07 23:29:06 -0300

    chore: using routes file

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,10 @@
 import express from 'express';
+import routes from './routes';
 
 const app = express();
 const PORT = 3333;
+
+app.use(routes);
 
 app.get('/', (req, res) => res.send('Express + TypeScript Server'));
 
