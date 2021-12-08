5b3dc7f999b06a551cf058542d5c9f4e2d968c5c
Pedro <pedro-gomes17@hotmail.com>
2021-12-08 00:20:17 -0300

    chore: added request and nextFunction to disappear typescript error in index main file

--- backend/src/middlewares/globalErrors.ts
+++ backend/src/middlewares/globalErrors.ts
@@ -1,7 +1,12 @@
-import { Response } from 'express';
+import { NextFunction, Request, Response } from 'express';
 import { AppError } from '../shared/error/AppError';
 
-export function globalErrors(error: Error, response: Response) {
+export function globalErrors(
+  error: Error,
+  request: Request,
+  response: Response,
+  next: NextFunction,
+) {
   if (error instanceof AppError) {
     response.status(error.statusCode).json({
       status: 'error',
