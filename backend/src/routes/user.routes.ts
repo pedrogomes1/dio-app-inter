e51b0532d6797d1faa2c3c78b41a0fdb769662bc
Pedro <pedro-gomes17@hotmail.com>
2021-12-08 23:48:12 -0300

    fix: correct name of the verb http in signin route

--- backend/src/routes/user.routes.ts
+++ backend/src/routes/user.routes.ts
@@ -4,7 +4,7 @@ import { UserControler } from '../resources/user/user.controller';
 const userRouter = Router();
 const userController = new UserControler();
 
-userRouter.get('/signin', userController.signIn);
+userRouter.post('/signin', userController.signIn);
 userRouter.post('/signup', userController.signUp);
 
 export { userRouter };
