786d1e59c92cf2dd55f167bbb94a433976fb2b09
Pedro <pedro-gomes17@hotmail.com>
2021-12-08 23:41:05 -0300

    feat: created pix controller with functions of request, pay and transactions

---
+++ backend/src/resources/pix/pix.controller.ts
@@ -0,0 +1,31 @@
+import { Request, Response } from 'express';
+import { PixService } from './pix.service';
+
+export class PixControler {
+  async request(request: Request, response: Response) {
+    const pixService = new PixService();
+
+    const { value } = request.body;
+    const user = request.user;
+
+    const requestKey = await pixService.request(value, user);
+    return response.status(200).send({ copyPasteKey: requestKey });
+  }
+
+  async pay(request: Request, response: Response) {
+    const pixService = new PixService();
+
+    const { key } = request.params;
+    const payment = await pixService.pay(key, request.user);
+
+    return response.status(201).send(payment);
+  }
+
+  async transactions(request: Request, response: Response) {
+    const pixService = new PixService();
+
+    const transactions = await pixService.transactions(request.user);
+
+    return response.status(201).send({ transactions });
+  }
+}
